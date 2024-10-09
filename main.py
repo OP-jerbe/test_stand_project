1e768c9a2b4ecff7ad0ea64edd04d689ac1aee84
OP-jerbe <j.erbe@oregon-physics.com>
2024-10-09 08:26:05 -0700

    Switch over to using PySide6 rather than PyQt6 due to licensing terms. Added functionality to app to actually talk to VRG. It works!

--- main.py
+++ main.py
@@ -1,7 +1,171 @@
+import sys
 from rfgenerator_control import RFGenerator
 import configparser
-from PyQt6.QtCore import Qsize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QCheckBox, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget
+
+def load_config(file_name):
+     config = configparser.ConfigParser()
+     config.read(file_name)
+     rf_com_port = config.get('RFGenerator', 'COMPort')
+     return rf_com_port
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ini_file = 'hyperionTestStandControl.ini'
+        self.rfg_com_port = load_config(ini_file)
+        self.resource_name = f'ASRL{rfg_com_port}::INSTR'
+        self.rfg = RFGenerator(self.resource_name)
+
+        self.setWindowTitle("VRG Control")
+        
+        # Enable switch
+        self.toggle_switch_label = QLabel('Enable RF', self)
+        self.toggle_switch_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.toggle_switch = QCheckBox("Enable", self)
+        self.toggle_switch.setChecked(False)
+        self.toggle_switch.stateChanged.connect(self.on_toggle)
+        self.toggle_switch.setStyleSheet("""
+            QCheckBox {
+                width: 50px;
+                height: 20px;
+                border: 2px solid #999999;
+                border-radius: 10px;
+                background: #e0e0e0;
+            }
+            QCheckBox:checked {
+                background: #76b041;
+                border: 2px solid #76b041;
+            }
+            QCheckBox:indicator {
+                width: 18px;
+                height: 18px;
+                border-radius: 9px;
+            }
+            QCheckBox:indicator:checked {
+                background: white;
+            }
+        """)
+        
+        # Frequency setting
+        self.top_freq_label = QLabel('Frequency (MHz)', self)
+        self.freq_setting_label = QLabel('Frequency = 00.00 MHz', self)
+        self.freq_setting_input = QLineEdit(self)
+        self.freq_setting_input.setMaxLength(5)
+        self.freq_setting_input.setPlaceholderText('Input Frequency Setting')
+        
+        # Power setting
+        self.top_power_label = QLabel('Power (W)', self)
+        self.power_setting_label = QLabel('Power = 0 W', self)
+        self.power_setting_input = QLineEdit(self)
+        self.power_setting_input.setMaxLength(4)
+        self.power_setting_input.setPlaceholderText('Input Power Setting')
+        
+        # Set up layout
+        toggle_switch_layout = QVBoxLayout()
+        toggle_switch_layout.addWidget(self.toggle_switch_label)
+        toggle_switch_layout.addWidget(self.toggle_switch)
+        
+        freq_layout = QVBoxLayout()
+        freq_layout.addWidget(self.top_freq_label)
+        freq_layout.addWidget(self.freq_setting_input)
+        freq_layout.addWidget(self.freq_setting_label)
+        freq_layout.setContentsMargins(10, 3, 10, 3)
+        
+        power_layout = QVBoxLayout()
+        power_layout.addWidget(self.top_power_label)
+        power_layout.addWidget(self.power_setting_input)
+        power_layout.addWidget(self.power_setting_label)
+        power_layout.setContentsMargins(10, 3, 10, 3)
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(toggle_switch_layout)
+        main_layout.addLayout(freq_layout)
+        main_layout.addLayout(power_layout)
+        main_layout.setContentsMargins(10, 10, 10, 10) # Set margins (left, top, right, bottom)
+        
+        container = QWidget()
+        container.setLayout(main_layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
+
+        # Connect QLineEdit's returnPressed signal to the update_label method with arguments
+        # A lambda function is required since arguments must be passed to the update_label method
+        self.freq_setting_input.returnPressed.connect(
+            lambda: self.update_setting(self.freq_setting_input,
+                                      self.freq_setting_label,
+                                      param='Frequency',
+                                      unit='MHz'
+            )
+        )
+
+        self.power_setting_input.returnPressed.connect(
+            lambda: self.update_setting(self.power_setting_input,
+                                      self.power_setting_label,
+                                      param='Power',
+                                      unit='W'
+            )
+        )
+        
+
+
+
+    def on_toggle(self, state):
+        if state == 2: # checked
+            print('Toggle Switch: ON') # replace this with command to enable
+            self.rfg.enable()
+        else: # unchecked
+            print('Toggle Swithc: OFF') # replace this with command to disable
+            self.rfg.disable()
+    
+    def update_setting(self, input_line:QLineEdit, label:QLabel, param:str, unit:str):
+        entered_text = input_line.text() # Get text from QLineEdit
+        if param == 'Frequency':
+            num = float(entered_text)
+            num_as_str = f'{num:.2f}'
+            label.setText(f'{param} = {num_as_str} {unit}') # Set text to QLabel
+            self.rfg.set_frequency(num)
+        elif param == 'Power':
+            num = float(entered_text)
+            num_as_str = f'{int(num)}'
+            label.setText(f'{param} = {num_as_str} {unit}') # Set text to QLabel
+            self.rfg.set_power(int(num))
+        else:
+            pass
+
+        
+
+if __name__ == '__main__':
+    ini_file = 'hyperionTestStandControl.ini'
+    rfg_com_port = load_config(ini_file)
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # =============================================================================
 # def load_config(file_name):
@@ -41,20 +205,3 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 # if __name__ == '__main__':
 #     main()
 # =============================================================================
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        
-        self.setWindowTitle('VRG Control')
-        button = QPushButton('Press Me!')
-        
-        self.setCentralWidget(button)
-
-app = QApplication([])
-window = MainWindow()
-window.show()
-
-app.exec()
-    

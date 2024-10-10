33fd21600373c7558e826ae65857194d39057b01
OP-jerbe <j.erbe@oregon-physics.com>
2024-10-10 06:53:19 -0700

    Split up main function for more flexibility in finding devices.

--- ini_reader.py
+++ ini_reader.py
@@ -1,8 +1,16 @@
 import configparser
 
-def load_config(file_name:str, header:str) -> str:
-     config = configparser.ConfigParser()
-     config.read(file_name)
-     com_port = config.get(header, 'COMPort')
-     print(f'{type(com_port) = }')
-     return com_port
+def load_config(file_name:str) -> dict:
+     config_data = configparser.ConfigParser()
+     config_data.read(file_name)
+     return config_data
+
+def find_comport_device(config_data:list, header:str) -> tuple:
+     device = config_data.get(header, 'device')
+     address = config_data.get(header, 'com_port')
+     return device, address
+
+def find_IP_device(config_data:list, header:str) -> tuple:
+     device = config_data.get(header, 'device')
+     address = config_data.get(header, 'IPaddress')
+     return device, address

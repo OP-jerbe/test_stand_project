30e97a9c2d00c0bde89f326c77db2186a3487be2
OP-jerbe <j.erbe@oregon-physics.com>
2024-10-11 15:05:46 -0700

    Added thread locking to each command so they don't interfere with each other. works great!

--- rfgenerator_control.py
+++ rfgenerator_control.py
@@ -1,4 +1,4 @@
-
+import threading
 
 class RFGenerator():
     def __init__(self, resource_name, rf_device_type=None):
@@ -8,6 +8,7 @@ class RFGenerator():
         self.power_setting = 0
         self.forward_power = 0
         self.refl_power = 0
+        self.lock = threading.Lock()
     
     def set_rf_device(self, rf_device_type, resource_name):
         accepted_devices = ['VRG']
@@ -21,18 +22,22 @@ class RFGenerator():
         self.rf_device = device(resource_name)
         
     def ping_device(self):
-        return self.rf_device.ping()
+        with self.lock:
+            return self.rf_device.ping()
     
     def enable(self):
-        self.rf_device.enable_RF()
-        self.enabled = True
+        with self.lock:
+            self.rf_device.enable_RF()
+            self.enabled = True
 
     def disable(self):
-        self.rf_device.disable_RF()
-        self.enabled = False
+        with self.lock:
+            self.rf_device.disable_RF()
+            self.enabled = False
     
     def close(self):
-        self.rf_device.close()
+        with self.lock:
+            self.rf_device.close()
 
 ###############################################################################
 ############################# getter methods ##################################
@@ -40,37 +45,45 @@ class RFGenerator():
 
     def get_frequency(self) -> float:
         """Method to ask RF generator what the frequency setting is."""
-        self.freq = self.rf_device.read_frequency()
-        return self.freq
+        with self.lock:
+            self.freq = self.rf_device.read_frequency()
+            return self.freq
 
     def get_power_setting(self) -> int:
         """Method to ask RF generator what the power setting is."""
-        self.power_setting = self.rf_device.read_power_setting()
-        return self.power_setting
+        with self.lock:
+            self.power_setting = self.rf_device.read_power_setting()
+            return self.power_setting
 
     def get_refl_power(self) -> int:
         """Method to ask RF generator how much reflected power is coming back"""
-        self.refl_power = self.rf_device.read_reflected_power()
-        return self.refl_power
+        with self.lock:
+            self.refl_power = self.rf_device.read_reflected_power()
+            return self.refl_power
 
     def get_forward_power(self) -> int:
-        self.forward_power = self.rf_device.read_forward_power()
-        return self.forward_power
+        with self.lock:
+            self.forward_power = self.rf_device.read_forward_power()
+            return self.forward_power
     
     def get_absorbed_power(self) -> int:
-        forward_power = self.rf_device.read_forward_power()
-        reflected_power = self.rf_device.read_reflected_power()
-        return forward_power - reflected_power
+        with self.lock:
+            forward_power = self.rf_device.read_forward_power()
+            reflected_power = self.rf_device.read_reflected_power()
+            return forward_power - reflected_power
 
 ###############################################################################
 ############################# setter methods ##################################
 ###############################################################################
         
     def set_frequency(self, freq: float):
-        self.rf_device.set_freq(freq)
+        with self.lock:
+            self.rf_device.set_freq(freq)
     
     def auto_tune(self):
-        self.rf_device.autotune()
+        with self.lock:
+            self.rf_device.autotune()
 
     def set_power(self, power: int):
-        self.rf_device.set_rf_power(power)
+        with self.lock:
+            self.rf_device.set_rf_power(power)

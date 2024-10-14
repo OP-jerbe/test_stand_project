072724c3ae02529967ca41361440d13d0b1ff9ad
OP-jerbe <j.erbe@oregon-physics.com>
2024-10-14 09:00:34 -0700

    removed unused files ammeter_control.py and pressure_gauge_control.py. updated pychache.

--- ammeter_control.py
+++
@@ -1,8 +0,0 @@
-
-import random
-
-def get_cup_current():
-    return random.gauss(500e-9, 5e-9)
-    
-def get_screen_current():
-    return random.gauss(32e-6, 1e-7)
--- pressure_gauge_control.py
+++
@@ -1,5 +0,0 @@
-
-import random
-
-def get_pressure():
-    return random.gauss(8e-7, 5e-8)

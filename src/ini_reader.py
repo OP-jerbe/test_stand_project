f4593837f2eda15adba1f6c5d94458ebb34978e9
OP-jerbe <j.erbe@oregon-physics.com>
2025-05-29 12:54:49 -0700

    Updates the ini_reader.py file to include the port. Also adds type hinting.

--- src/ini_reader.py
+++ src/ini_reader.py
@@ -10,22 +10,23 @@ def load_config(file_name: str) -> ConfigData:
     return config_data
 
 
-def find_comport_device(config_data: ConfigData, header: str) -> tuple:
+def find_comport_device(config_data: ConfigData, header: str) -> tuple[str, str]:
     device = config_data.get(header, 'device')
     address = config_data.get(header, 'com_port')
     return device, address
 
 
-def find_IP_device(config_data: ConfigData, header: str) -> tuple:
+def find_IP_device(config_data: ConfigData, header: str) -> tuple[str, str, str]:
     device = config_data.get(header, 'device')
-    address = config_data.get(header, 'IPaddress')
-    return device, address
+    ip = config_data.get(header, 'ip')
+    port = config_data.get(header, 'port')
+    return device, ip, port
 
 
 if __name__ == '__main__':
     ini_file = 'hyperionTestStandControl.ini'
     config_data = load_config(ini_file)
     rf_device, com_port = find_comport_device(config_data, 'RFGenerator')
-    hvps_device, ip = find_IP_device(config_data, 'HVPS')
+    hvps_device, hvps_ip, hvps_port = find_IP_device(config_data, 'HVPS')
     print(f'{rf_device = }\n{com_port = }')
-    print(f'{hvps_device = }\n{ip = }')
+    print(f'{hvps_device = }\n{hvps_ip = }\n{hvps_port = }')
